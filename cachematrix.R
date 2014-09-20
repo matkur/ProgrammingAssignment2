4bc33814c04f86dd8edece38514a5acd1845e1fe
matkur <matti.x.kurki@hotmail.fi>
2014-09-20 18:49:53 +0300

    Final Test

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## With these two functions (makeCacheMatrix and cacheSolve) a matrix can be inverted and the inversion is cached to be
-## used again. It is assumed that there is an inverse of the matrix.
-## Call first makeCacheMatrix with argument matrix to be inverted. This function returns a list of fuction pointers that are 
-## used in cacheSolve funtion.
+## With these two functions (makeCacheMatrix and cacheSolve) a matrix can be inverted and the inversion is cached 
+## for further use. It is assumed that the matrix is a square matrix and that there is an inverse of the matrix.
+##
+## Call first makeCacheMatrix with argument the matrix to be inverted. This function returns a list of fuction pointers
+## that are used in cacheSolve funtion.
+##
 ## Example of the use of these function calls:
 ## m<-makeCacheMatrix(matrix(1:4,2,2))  
-## cacheSolve(m)                         ## this call inverts the matrix and retuns the inverted matrix
-## cacheSolve(m)                         ## the next call uses the cached inverted matrix and returns it
+## cacheSolve(m)                         ## this call calculates the inverse matrix and returns it
+## cacheSolve(m)                         ## the next cacheSolve call returns the cached inverted matrix and returns it
 ##                                       ## and you get the message "getting cached data"
-## Your program can call the functions inside makeCacheMatrix function directly, but then the results are
-## not guaranted to be correct !!
+##
+## Although your program can call the functions inside makeCacheMatrix directly, the results might be wrong !!
+## If you use a matrix that is not a square matrix or that has no inverse, you will get error message from the "solve"
+## function.
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
@@ -28,47 +32,47 @@ makeCacheMatrix <- function(x = matrix()) {
         x
     } 
     
-    #setInverse sets inverse of x into variable inv_matrix (this matrix is returned in cacheSolve to the calling program)
+    #setInverse sets inverse of x into variable inv_matrix
     setInverse <- function(i_matrix){
         inv_matrix <<- i_matrix 
     }
     
-    # getInverse returns the matrix of inv_matrix (from makeCacheMatrix)
+    # getInverse returns the inverted matrix
     getInverse <- function() {
         inv_matrix
     }
     
-    #returns a labeled vector of functions set, get, setmean and getmean
+    #makeCacheMatrix returns a labeled vector of functions setMatrix, getMatrix, setInverse, getInverse
     list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }    
 
 
-## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix funtion above.
+## cacheSolve: This function computes the inverse of the special "matrix" returned from makeCacheMatrix getInverse function
 ## If the inverse has already been calculated for this matrix, then this function retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of the argument matrix 'x'
+   
     
-    #attempts to get the inverted matrix from x (if it was inverted previously)
+    # attempts to get the inverted matrix from x (if it was inverted previously)
     inv_matrix <- x$getInverse()
     
-    #if not null, an inverted matrix was cached, so return inv_matrix 
+    # if not null, an inverted matrix was cached, so return it and give a message 
     if(!is.null(inv_matrix)) {
         message("getting cached data")
         return(inv_matrix)
     }
     
-    #since the inverted matrix is null, set arg_matrix to the matrix from makeCacheMatrix
+    # since the inverted matrix is null, set arg_matrix to the orginal matrix from makeCacheMatrix
     
     arg_matrix <- x$getMatrix()
     
-    #Invert the matrix. In this cacheSolve function it is assumed that the matrix supplied is always invertible.
+    # invert the matrix. Here we assume that the matrix used is always invertible and is a square matrix
     inv_matrix <- solve(arg_matrix, ...)
     
     #set inverted matrix to inv_matrix
     x$setInverse(inv_matrix)
     
-    #return inverse
+    ## Return a matrix that is the inverse of the argument matrix 'x'
     inv_matrix    
     
 }
